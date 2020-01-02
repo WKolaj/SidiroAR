6c767670f6e34233dfebdd1aacff0c7dd12ef8c9
Witold Kolaj <WKolaj@gmail.com>
2020-01-02 11:23:53 +0100

    Added test file server

--- Assets/DataFiles/Classes/Asset Bundle Service/AssetModelService.cs
+++ Assets/DataFiles/Classes/Asset Bundle Service/AssetModelService.cs
@@ -1,7 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class AssetModelService
 {
+    static string fileApiURL = "http://localhost:5000/api/models";
+
+    public static WebClient DownloadAssembly(string userId, string assemblyId, string filePath)
+    {
+        var webClient = new WebClient();
+
+        webClient.DownloadFileAsync(new Uri(String.Format("{0}/{1}/{2}", fileApiURL,userId, assemblyId)), filePath);
+
+        return webClient;
+    }
 }
