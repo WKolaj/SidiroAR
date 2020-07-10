ab5e1a9df72bef09a252f936a63fcc0045cb8014
Witold Kolaj <WKolaj@gmail.com>
2020-07-10 16:43:45 +0200

    Updated Dialog Window

---
+++ Assets/Scenes/DataFiles/Common/DialogWindow/DialogWindow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DialogWindow : MonoBehaviour
+{
+    [SerializeField]
+    private float _windowWidthPercentage = 50;
+    public float WindowWidthPercentage
+    {
+        get
+        {
+            return _windowWidthPercentage;
+        }
+
+        set
+        {
+            _windowWidthPercentage = value;
+        }
+    }
+
+    private RectTransform _rectTrans = null;
+
+    private GameObject _windowGO = null;
+    private GameObject _textLabelGO = null;
+    private GameObject _buttonContainerGO = null;
+
+    private RectTransform _windowRectTrans = null;
+    private RectTransform _textLabelRectTrans = null;
+    private RectTransform _buttonContainerRectTrans = null;
+
+    private TextMeshProUGUI _textLabelTMP = null;
+
+    private void Awake()
+    {
+        this._rectTrans = this.GetComponent<RectTransform>();
+
+        this._windowGO = this.transform.Find("Window").gameObject;
+        this._textLabelGO = _windowGO.transform.Find("TextLabel").gameObject;
+        this._buttonContainerGO = _windowGO.transform.Find("ButtonContainer").gameObject;
+
+        this._windowRectTrans = _windowGO.GetComponent<RectTransform>();
+        this._textLabelRectTrans = _textLabelGO.GetComponent<RectTransform>();
+        this._buttonContainerRectTrans = _buttonContainerGO.GetComponent<RectTransform>();
+
+        this._textLabelTMP = _textLabelGO.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        _setWindowWidth();
+    }
+
+    private void _setWindowWidth()
+    {
+        var currentWidth = _rectTrans.rect.width;
+
+        var widthToSet = currentWidth * WindowWidthPercentage / 100;
+
+        this._windowRectTrans.sizeDelta = new Vector2(widthToSet, this._windowRectTrans.sizeDelta.y);
+    }
+
+}
---
+++ Assets/Scenes/DataFiles/Common/DialogWindow/DialogWindowButton/DialogWindowButton.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class DialogWindowButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button.ButtonClickedEvent _onClick = null;
+    public Button.ButtonClickedEvent OnClick
+    {
+        get
+        {
+            return _onClick;
+        }
+
+        set
+        {
+            _onClick = value;
+        }
+    }
+
+    [SerializeField]
+    private float _labelFontSize = 56;
+    public float LabelFontSize
+    {
+        get
+        {
+            return _labelFontSize;
+        }
+
+        set
+        {
+            _labelFontSize = value;
+        }
+    }
+
+
+    [SerializeField]
+    private Color _labelFontColor = Color.white;
+    public Color LabelFontColor
+    {
+        get
+        {
+            return _labelFontColor;
+        }
+
+        set
+        {
+            _labelFontColor = value;
+        }
+    }
+
+    [SerializeField]
+    private string _labelText = "Button label";
+    public string LabelText
+    {
+        get
+        {
+            return _labelText;
+        }
+
+        set
+        {
+            _labelText = value;
+        }
+    }
+
+
+    private GameObject _labelGO = null;
+    private GameObject _buttonMaskGO = null;
+
+    private RectTransform _labelRectTrans = null;
+    private RectTransform _buttonRectTrans = null;
+
+    private TextMeshProUGUI _labelTMP  = null;
+    private DialogWindowButtonMask _buttonMask = null;
+
+
+    private void Awake()
+    {
+        this._labelGO = this.transform.Find("Label").gameObject;
+        this._buttonMaskGO = this.transform.Find("ButtonMask").gameObject;
+
+        this._labelRectTrans = this._labelGO.GetComponent<RectTransform>();
+        this._buttonRectTrans = this._buttonMaskGO.GetComponent<RectTransform>();
+
+        this._labelTMP = this._labelGO.GetComponent<TextMeshProUGUI>();
+        this._buttonMask = this._buttonMaskGO.GetComponent<DialogWindowButtonMask>();
+
+        this._buttonMask.OnClick = this.OnClick;
+    }
+
+    private void Start()
+    {
+        _labelTMP.text = LabelText;
+        _labelTMP.color = LabelFontColor;
+        _labelTMP.fontSize = LabelFontSize;
+    }
+
+
+
+}
