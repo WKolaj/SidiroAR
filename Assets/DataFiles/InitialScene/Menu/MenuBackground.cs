fe9d1218510fd6b2d060f9111e2c38f32d7e3528
Witold Kolaj <WKolaj@gmail.com>
2020-07-17 15:29:00 +0200

    Added menu hidding after clicking background

---
+++ Assets/DataFiles/InitialScene/Menu/MenuBackground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuBackground : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Hidding menu when background was clicked
+        MainCanvas.HideMenu();
+    }
+}
