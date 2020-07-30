ba79fd290b448e5c4cbfcce8cb5fa94c5ce29324
Witold Kolaj <WKolaj@gmail.com>
2020-07-30 12:25:38 +0200

    Added support for DEMO Accounts access + Changed languge button text

---
+++ Assets/DataFiles/InitialScene/LoginPage/DemoButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button.ButtonClickedEvent _onClick = null;
+    public Button.ButtonClickedEvent OnClick
+    {
+        get
+        {
+            return _onClick;
+        }
+
+        set
+        {
+            _onClick = value;
+        }
+    }
+
+    private GameObject _loginButtonMaskGO;
+    private GameObject _backgroundGO;
+    private GameObject _textLabelGO;
+
+    private LoginButtonMask _loginButtonMask;
+    private Image _background;
+    private TextMeshProUGUI _textLabel;
+
+
+    private void Awake()
+    {
+        this._loginButtonMaskGO = transform.Find("ButtonMask").gameObject;
+        this._backgroundGO = transform.Find("Background").gameObject;
+        this._textLabelGO = transform.Find("Label").gameObject;
+
+        this._loginButtonMask = _loginButtonMaskGO.GetComponent<LoginButtonMask>();
+        this._background = _backgroundGO.GetComponent<Image>();
+        this._textLabel = _textLabelGO.GetComponent<TextMeshProUGUI>();
+
+        this._loginButtonMask.OnClick = this.OnClick;
+    }
+
+    public void SetText(string text)
+    {
+        this._textLabel.text = text;
+    }
+}
--- Assets/DataFiles/InitialScene/LoginPage/LoginPage.cs
+++ Assets/DataFiles/InitialScene/LoginPage/LoginPage.cs
@@ -9,6 +9,7 @@ public class LoginPage : MonoBehaviour
     private GameObject _loginInputGO;
     private GameObject _passwordInputGO;
     private GameObject _loginButtonGO;
+    private GameObject _demoButtonGO;
     private GameObject _languageButtonGO;
     private GameObject _loginInputPlaceholderGO;
     private GameObject _passwordInputPlaceholderGO;
@@ -17,6 +18,7 @@ public class LoginPage : MonoBehaviour
     private TMP_InputField _loginInput;
     private TMP_InputField _passwordInput;
     private LoginButton _loginButton;
+    private DemoButton _demoButton;
     private ClickableImage _languageButton;
     private TextMeshProUGUI _loginInputPlaceholder;
     private TextMeshProUGUI _passwordInputPlaceholder;
@@ -35,6 +37,7 @@ public class LoginPage : MonoBehaviour
         this._passwordInputGO = loginComponent.transform.Find("PasswordInput").gameObject;
         this._loginButtonGO = loginComponent.transform.Find("LoginButton").gameObject;
         this._mainLabelGO = loginComponent.transform.Find("MainLabel").gameObject;
+        this._demoButtonGO = loginComponent.transform.Find("DemoButton").gameObject;
 
         var loginInputTextArea = _loginInputGO.transform.Find("Text Area").gameObject;
         _loginInputPlaceholderGO = loginInputTextArea.transform.Find("Placeholder").gameObject;
@@ -46,11 +49,13 @@ public class LoginPage : MonoBehaviour
         this._passwordInput = _passwordInputGO.GetComponent<TMP_InputField>();
         this._loginButton = _loginButtonGO.GetComponent<LoginButton>();
         this._mainLabel = _mainLabelGO.GetComponent<TextMeshProUGUI>();
+        this._demoButton = _demoButtonGO.GetComponent<DemoButton>();
 
         this._loginInputPlaceholder = _loginInputPlaceholderGO.GetComponent<TextMeshProUGUI>();
         this._passwordInputPlaceholder = _passwordInputPlaceholderGO.GetComponent<TextMeshProUGUI>();
 
         this._loginButton.OnClick.AddListener(_handleLoginButtonClicked);
+        this._demoButton.OnClick.AddListener(_handleDemoButtonClicked);
 
         this._languageButton.OnClick.AddListener(_handleLanguageButtonClicked);
     }
@@ -77,6 +82,29 @@ public class LoginPage : MonoBehaviour
         }
     }
 
+    private async void _handleDemoButtonClicked()
+    {
+        var emailText = Translator.GetTranslation("LoginPage.DemoAccountEmail");
+        var emailPassword = Translator.GetTranslation("LoginPage.DemoAccountPassword");
+
+        var logInSuccessfull = await MainCanvas.AsyncTryLogIn(emailText, emailPassword);
+
+        this._loginInput.text = "";
+
+        this._passwordInput.text = "";
+
+        if (logInSuccessfull)
+        {
+            //Setting language if exists in possible languages
+            if (UserLoader.LoggedUser.DefaultLang != null && Translator.GetPossibleLangs().Contains(UserLoader.LoggedUser.DefaultLang))
+                Translator.SetLang(UserLoader.LoggedUser.DefaultLang);
+
+            //Hidding aux page
+            MainCanvas.HideAuxPage();
+        }
+    }
+
+
     private bool _validateLoginAndPassword()
     {
         if (string.IsNullOrEmpty(this._loginInput.text) || string.IsNullOrEmpty(this._passwordInput.text)) return false;
@@ -99,6 +127,7 @@ public class LoginPage : MonoBehaviour
         _passwordInputPlaceholder.text = Translator.GetTranslation("LoginPage.PasswordInputPlaceholder");
         _loginButton.SetText(Translator.GetTranslation("LoginPage.LoginButtonText"));
         _mainLabel.text = Translator.GetTranslation("LoginPage.MainLabelText");
+        _demoButton.SetText(Translator.GetTranslation("LoginPage.DemoButtonText"));
     }
 
 }
